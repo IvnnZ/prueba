3dbfc7622c488ec5364b092d3e5e7b5c89da7366
Ivnn <ivanevolution.lmz@gmail.com>
2020-02-06 12:12:36 +0100

    Una nueva actualización
    Signed-off-by: Ivnn <ivanevolution.lmz@gmail.com>

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hola mundo");
+		System.out.println("Buenos dias desde Cuba");
+		System.out.println("Y viva la revolución");
 
 	}
 
